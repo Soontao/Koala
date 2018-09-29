02f25f816dd767b854f9dc237fd6e3da96beb297
Theo Sun <theo.sun@sap.com>
2018-09-29 15:43:41 +0800

    full refactor

--- src/test/java/org/fornever/koala/KoalaTest.java
+++ src/test/java/org/fornever/koala/KoalaTest.java
@@ -1,20 +1,12 @@
 package org.fornever.koala;
 
-import java.util.Map;
-
-import org.fornever.koala.impl.Koala;
-import org.fornever.koala.types.MobilePhone;
-import org.fornever.koala.types.enums.EConsistenceLevel;
-import org.fornever.koala.types.internal.KoalaConfig;
 import org.junit.Test;
 
 public class KoalaTest {
 
 	@Test
 	public void test() {
-//		KoalaConfig<MobilePhone, Long, Map<String, String>> config = new KoalaConfig<>();
-//		Koala<MobilePhone, Long, Map<String, String>> koala = Koala.New(config);
-//		
+		
 	}
 
 }
